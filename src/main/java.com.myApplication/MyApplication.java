5ea63b1c66c84f245b18737abb5ee845ff9f87be
Vlad <vladfilimontsev@yandex.ru>
2025-08-20 12:57:34 +0300

    From new Branch

--- src/main/java.com.myApplication/MyApplication.java
+++ src/main/java.com.myApplication/MyApplication.java
@@ -2,5 +2,6 @@ public class MyApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Bye World");
+        System.out.println("Hello From new Branch");
     }
 }
